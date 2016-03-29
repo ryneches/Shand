293f708295e207c493f06b76aa6d27b85c4a0dd3
Russell Neches <git@vort.org>
2016-03-29 00:41:52 -0700

    queue debugging, continued

--- shand/shand.py
+++ shand/shand.py
@@ -154,7 +154,7 @@ class Problem(object) :
                     done_queue.put(t)
                 except AssertionError :
                     done_queue.put(False)
-                print t['pid'] + ' did something...'       
+            print t['pid'] + ' finished.'
  
         work_queue = Queue()
         done_queue = Queue()
